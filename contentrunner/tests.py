363df7d74263325e8672a44f3885f0d5821ac8dc
mcheich <mcheich@gmail.com>
2021-09-25 06:22:15 -0400

    added first dummy view and URL mapping

--- contentrunner/tests.py
+++ contentrunner/tests.py
@@ -1,6 +1,14 @@
+from django.http import response
+from django.test.testcases import _AssertTemplateUsedContext
+from django.urls import resolve
 from django.test import TestCase
+from contentrunner.views import home_page
 
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+        # response = self.client.get('/')
+        # self.assertTemplateUsed(response, 'home.html')
