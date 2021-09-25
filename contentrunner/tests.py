a19d3f2243d88e5c2580cdfd7b624cd235178bb8
mcheich <mcheich@gmail.com>
2021-09-25 06:43:39 -0400

    home page view function started

--- contentrunner/tests.py
+++ contentrunner/tests.py
@@ -1,8 +1,10 @@
-from django.http import response
+#  from django.http import request, response 
+from django.http import HttpRequest
 from django.test.testcases import _AssertTemplateUsedContext
 from django.urls import resolve
 from django.test import TestCase
 from contentrunner.views import home_page
+
 
 
 class HomePageTest(TestCase):
@@ -10,5 +12,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-        # response = self.client.get('/')
-        # self.assertTemplateUsed(response, 'home.html')
+        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>ContentRunner</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- contentrunner/views.py
+++ contentrunner/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>ContentRunner</title></html>')
